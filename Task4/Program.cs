56bbb7c23bff284d721ca7ed49b6598be6425c00
Aysylu23 <aysbad25@gmail.com>
2023-10-13 15:41:27 +0500

    Add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+на само себя).
+
+Например:
+4 -> 16 
+-3 -> 9 
+-7 -> 49
+*/
+
+Console.Write("Введите число: ");
+string? input =  Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int sqrtNumber = number * number;
+
+Console.Write("Число " + number + " в квадрате равно: ");
+Console.WriteLine(sqrtNumber);
