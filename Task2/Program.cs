ef2244ea3d2dafeeaa9ee0a23738beb6adce37a9
Aysylu23 <aysbad25@gmail.com>
2023-10-13 15:22:41 +0500

    Add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Напишите программу, которая при введении чисел будет выдавать дни недели*/
+Console.Write("Введите первое число: ");
+string? input =  Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Такого дня недели нет");
+}
