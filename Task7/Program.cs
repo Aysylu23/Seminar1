7a003024dbeefd871935c492c3abc9cb41ff6cea
Aysylu23 <aysbad25@gmail.com>
2023-10-13 15:32:51 +0500

    Add task7

---
+++ Task7/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8
+*/
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99)
+{
+    if(number < 1000)
+    {
+        int result = number % 10;
+        Console.WriteLine(result);
+    }
+}
