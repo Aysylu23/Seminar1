026055d2da3da61db2a7819c27767e81c807a41c
Aysylu23 <aysbad25@gmail.com>
2023-10-14 22:28:51 +0500

    Add homework8

---
+++ Homework8/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N*/
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int chNumber = 2;
+
+while (chNumber <= n)
+{
+    Console.Write(chNumber + " ");
+    chNumber += 2;
+}
